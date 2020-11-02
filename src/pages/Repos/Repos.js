87d708843f81c3b76a8a7e7dea0afc5d190b964b
unknown <leo_faleiros@hotmail.com>
2020-11-02 00:05:45 -0300

    Finalizando add / remove star

--- src/pages/Repos/Repos.js
+++ src/pages/Repos/Repos.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { useFetch } from '../../hooks/useFetch';
+import axios from 'axios';
+import { Link } from 'react-router-dom';
 import { toast, ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.min.css'; 
+import { FiGithub } from 'react-icons/fi';
+import { AiFillStar, AiOutlineStar } from 'react-icons/ai';
 import './style.css'
 
 export default function Home() {
@@ -26,7 +30,26 @@ export default function Home() {
       toast.success('Redirecionando ao repositorio...');
       window.open(url)
     })
-    
+  }
+
+  async function favoriteRepo(stars, name) {
+    console.log(stars, name);
+
+    if(stars === 0) {
+      try {
+        await axios.put(`https://api.github.com/user/starred/${user}/${name}`, {
+          headers: {
+            "Accept": "application/vnd.github.v3+json",
+            "Content-Length": "0"
+          }
+        })
+          .then(function(response) {
+            console.log(response);
+          })
+      } catch (error) {
+        console.log(`${error}`);
+      }
+    }
   }
 
   return (
@@ -36,11 +59,12 @@ export default function Home() {
           <h1>Repositórios de {user}</h1>
         </div>
           {data.map(dados => ( 
-            <div className="repos-container" key={dados.id} onClick={() => goToRepo(dados.html_url)}>
+            <div className="repos-container" key={dados.id}>
               <div className="item image info-container">
                 <img src={dados.owner.avatar_url} alt={dados.id}/>
               </div>
 
+              
               <div className="item conteudo info-container">
                 <b>Nome: </b> <p>{dados.name}</p>
               </div>
@@ -108,6 +132,24 @@ export default function Home() {
                     <span> {dt}/{month}/{year} {hours}:{minutes}:{seconds}</span>
                   }
                 </p>
+              </div>
+
+              <div className="info-container star">
+                <p>
+                  {
+                  dados.stargazers_count === 0
+                  ? <span onClick={() => favoriteRepo(dados.stargazers_count, dados.name)} >
+                      <AiOutlineStar size={20} /> &nbsp;Não favorito
+                    </span> 
+                  : <span onClick={() => favoriteRepo(dados.stargazers_count, dados.name)}>
+                      <AiFillStar size={20} /> &nbsp;Favorito
+                    </span>    
+                  }
+                  </p>
+              </div>
+
+              <div className="item conteudo info-container">
+                <Link onClick={() => goToRepo(dados.html_url)}><FiGithub color="#000" size={20} /> &nbsp;Visite o repositório...</Link>
               </div>
             </div>
           ))}
